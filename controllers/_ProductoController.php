21f5897d5190a00c785196421bf8907d64e3fccc
d4rksit3 <gonzaloroque21@gmail.com>
2024-07-09 16:55:12 -0500

    jroque

--- controllers/_ProductoController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ProductoController extends Controller {
-    public function index() {
-        $productoModel = $this->loadModel('Producto');
-        $productos = $productoModel->getAllProductos();
-        $this->loadView('admin/productos', ['productos' => $productos]);
-    }
-}
-?>
